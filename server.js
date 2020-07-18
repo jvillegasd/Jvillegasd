63ace16c0020d2dd82d152827ad773fd62c14030
C9-LinkRs <linkrsfan@gmail.com>
2020-07-18 23:28:50 +0200

    Component almost done. Have to link it to spotify

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const body_parser = require("body-parser");
+const path = require("path");
+const favicon = require("serve-favicon");
 const spotify = require("./spotify");
 
 const app = express();
 
 app.use(body_parser.json());
 
+// Setting views and static files
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use('/public', express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+
+// Client stuff
 app.get("/", (request, response) => {
+  response.render("music_player", {
+    image_link: "/public/images/no_song.png",
+    progress: "3:00",
+    duration: "3:00",
+    progress_percentage: 100,
+    title: "Fetching data...",
+    artist: "Someone",
+    spotify_link: "#"
+  });
+});
+
+// Server stuff
+app.get("/api/", (request, response) => {
   response.status(200).json({ message: "Server's up" });
 });
 
